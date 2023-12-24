5eaa80b7eb2607a3949abf92a97f1abbfd3a3afd
Ian Miller <imiller142@gmail.com>
2023-12-24 11:29:25 -0600

    Added get request and front end stuff

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+//allow css and js
+app.use(express.static('public'));
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -89,6 +92,24 @@ function createNewAnimal(body, animalsArray) {
 
   return animal;
 }
+
+//==================Routes================
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
